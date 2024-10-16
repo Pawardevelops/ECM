ea07e7d397a17008cb2862a5d40c9f29a3727b82
Pawardevelops <pawardevelops@gmail.com>
2024-10-16 17:02:55 +0530

    intend for main

--- app/src/main/java/com/example/mec/MainActivity.java
+++ app/src/main/java/com/example/mec/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.mec;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        AppCompatButton adminButton = findViewById(R.id.enterAdminBttn);
+        AppCompatButton candidateButton = findViewById(R.id.enterCandidateBttn);
+        AppCompatButton voterButton = findViewById(R.id.enterVoterBttn);
+
+        // Set onClickListeners for each button
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to AdminLoginActivity
+                Intent intent = new Intent(MainActivity.this, adminlogin.class);
+                startActivity(intent);
+            }
+        });
+
+        candidateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to CandidateLoginActivity
+                Intent intent = new Intent(MainActivity.this, candidateLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        voterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to VoterLoginActivity
+                Intent intent = new Intent(MainActivity.this, voters_login.class);
+                startActivity(intent);
+            }
         });
     }
 }
